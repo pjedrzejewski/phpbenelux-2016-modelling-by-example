b19989ae5df8056b52dc4275c13c71967dcd4fc1
Paweł Jędrzejewski <pawel@sylius.org>
2016-01-28 11:47:46 +0100

    Revert to [Step 01] Fresh start

--- features/bootstrap/UiContext.php
+++ features/bootstrap/UiContext.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Domain\Book;
-use App\Domain\BookTitle;
-use App\Domain\Isbn;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\MinkExtension\Context\MinkContext;
@@ -12,41 +9,13 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 class UiContext extends MinkContext implements KernelAwareContext, SnippetAcceptingContext
 {
-    private $library;
     private $entityManager;
 
     public function setKernel(KernelInterface $kernel)
     {
         $container = $kernel->getContainer();
 
-        $this->library = $container->get('app.library');
         $this->entityManager = $container->get('doctrine.orm.default_entity_manager');
-    }
-
-    /**
-     * @Given a book with ISBN :isbn and title :title was added to the library
-     */
-    public function aBookWithIsbnAndTitleWasAddedToTheCatalog($isbn, $title)
-    {
-        $this->library->add(Book::withTitleAndIsbn(new BookTitle($title), new Isbn($isbn)));
-    }
-
-    /**
-     * @When I search library by ISBN number :isbn
-     */
-    public function iSearchCatalogByIsbnNumber($isbn)
-    {
-        $this->visit('/search');
-        $this->fillField('ISBN', $isbn);
-        $this->pressButton('Search');
-    }
-
-    /**
-     * @Then I should find a single book
-     */
-    public function iShouldFindASingleBook()
-    {
-        $this->assertPageContainsText('Found 1 result(s).');
     }
 
     /**
--- src/Domain/BookInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Domain;
-
-interface BookInterface
-{
-    public function title();
-    public function isbn();
-}
